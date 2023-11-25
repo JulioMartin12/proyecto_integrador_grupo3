917a5183ed3f3d4825957acbfc2e2b27f4d69592
Julio Martin <faria@DESKTOP-TLUUL68>
2023-11-24 22:37:50 -0300

    Add: new files
    se agregaron la estructura del patron DAO
    se modifico el diagrama DER

--- src/main/java/org/example/DAO/AbstractJpaDAO.java
+++ src/main/java/org/example/DAO/AbstractJpaDAO.java
@@ -1,4 +1,48 @@
 package org.example.DAO;
 
-public class AbstractJpaDAO {
+import org.example.config.DBConfig;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+import java.io.Serializable;
+import java.util.List;
+
+public abstract class AbstractJpaDAO< T extends Serializable> {
+    private Class< T > clazz;
+
+    EntityManager entityManager = DBConfig.getEntityManager();
+
+    public final void setClazz( Class< T > clazzToSet ){
+        this.clazz = clazzToSet;
+    }
+
+    public T findOne( int id ){
+        return entityManager.find( clazz, id );
+    }
+    public List< T > findAll(){
+        return entityManager.createQuery( "from " + clazz.getName() )
+                .getResultList();
+    }
+
+    public void create( T entity ){
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        entityManager.persist( entity );
+        tx.commit();
+    }
+
+    public T update( T entity ){
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        T entityMerged = entityManager.merge( entity );
+        tx.commit();
+        return entityMerged;
+    }
+
+    public void delete( T entity ){
+        EntityTransaction tx = entityManager.getTransaction();
+        tx.begin();
+        entityManager.remove( entity );
+        tx.commit();
+    }
 }
--- src/main/java/org/example/DAO/DAO.java
+++ src/main/java/org/example/DAO/DAO.java
@@ -1,4 +1,13 @@
 package org.example.DAO;
 
-public interface DAO {
+import java.util.Collection;
+import java.util.Optional;
+
+public interface DAO<T> {
+
+    Optional<T> get(int id);
+    Collection<T> getAll();
+    int save(T t);
+    void update(T t);
+    void delete(T t);
 }
