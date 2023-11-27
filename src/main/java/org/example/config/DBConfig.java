7b6db4ae408d7e1f2ab1f1fdecdc17bf73738d55
leonnelc <78927152+leonnelc@users.noreply.github.com>
2023-11-27 13:14:06 -0300

    Cambios en persistence.xml para que la db se genere automaticamente, eliminados scripts sql innecesarios.

--- src/main/java/org/example/config/DBConfig.java
+++ src/main/java/org/example/config/DBConfig.java
@@ -3,17 +3,10 @@ package org.example.config;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import  java.sql.Statement;
 
 public class DBConfig {
 
     static EntityManagerFactory factory = null;
-    public static void generateSchema(){
-        Persistence.generateSchema("JPA_PU",null);
-    }
     public static EntityManager getEntityManager(){
         if(factory == null)
             factory = Persistence.createEntityManagerFactory("JPA_PU");
